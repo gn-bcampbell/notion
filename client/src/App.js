ef4cbbcaf2191c1a599479124b451cc551f1a67e
Brendan <b.campbell787@gmail.com>
2022-10-02 22:05:02 +0100

    Install tailwindcss 'class' config to apply on click

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Users from './components/User';
+import Home from './components/Home';
 
 function App() {
   return (
-    <Users />
+
+    <div className='App'>
+      <Users />
+      <Home />
+    </div>
   );
 }
 
